08952242bdfa4ad8421842ecacc923116226baca
mustafa <mustafazaman360@gmail.com>
2025-03-22 22:55:32 +0500

    Add Swagger setup in development mode

--- src/app.ts
+++ src/app.ts
@@ -4,11 +4,13 @@ import { setupSwagger, registerRoutes } from "staller-plugin";
 const PORT = 3000;
 const app = express();
 app.use(express.json());
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(registerRoutes({ rootDir: __dirname }));
-app.use(setupSwagger({ rootDir: __dirname }));
+
+if(process.env.NODE_ENV === 'development'){
+  app.use(setupSwagger({ rootDir: __dirname }));
+}
 
 app.listen(PORT, () =>
   console.log(`🚀 Server running on http://localhost:${PORT}`)
