2089ac02400261651087a9f4aece07c29f0931f0
mustafaa-zaman <mustafazaman360@gmail.com>
2025-06-16 21:08:41 +0500

    fix: types in controller

--- src/controllers/petsController.ts
+++ src/controllers/petsController.ts
@@ -1,18 +1,18 @@
 import { Controller, Get, Post, Tags, Summary, Body } from "staller-plugin";
-
+import { Request, Response } from "express";
     @Tags("pets")
     @Controller("/pets")
     export default class petsController {
         @Get("/:id?")
         @Summary("Get pets by ID or all")
-        get(req: any, res: any) {
+        get(req: Request, res: Response) {
             res.json(req.params.id ? { id: req.params.id, name: "buddy" } : [{ id: "1", name: "buddy" }]);
         }
 
         @Post("/")
         @Summary("Create a new pets")
         @Body({ name: { type: "string", required: true } })
-        create(req: any, res: any) {
+        create(req: Request, res: Response) {
             res.status(201).json({ id: "2", name: req.body.name });
         }
     }
